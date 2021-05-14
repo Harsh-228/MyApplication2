02216a1cdb198e1df2fd820d817015c8d962f689
anand228 <anandsrivastav228@gmail.com>
2021-05-14 18:15:12 +0530

    added text passing through intent

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String name= intent.getStringExtra("NAME");
+
+        TextView mresult = findViewById(R.id.textView2);
+        mresult.setText("Name =" + name);
     }
 }
